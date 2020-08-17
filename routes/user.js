b4633351895596367da6f24632a6c122d215ffc7
plaban <plabanmondal02@gmail.com>
2020-08-17 22:18:39 +0530

    update user route

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { isSignedIn, isAuthenticated } = require("../controllers/auth");
+const { getUser } = require("../controllers/user");
 const router = express.Router();
 
 // params
 router.param("userId", getUserById);
 
 // show user
-router.get("/user/:userId", isSignedIn, isAuthenticated);
+router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
 
 module.exports = router;
