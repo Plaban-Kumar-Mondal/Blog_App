a1cb08a69c5bd5d6db1ea84f220d0c023d1478ed
plaban <plabanmondal02@gmail.com>
2020-08-17 13:46:40 +0530

    AuthZ middleware add

--- controllers/auth.js
+++ controllers/auth.js
@@ -82,3 +82,14 @@ exports.isSignedIn = expressJwt({
   userProperty: "auth", // attached to req, res.auth
   algorithms: ["HS256"],
 });
+
+// authZ middleware
+exports.isAuthenticated = (req, res, next) => {
+  let checker = req.profile && req.auth && req.profile._id === req.auth._id;
+  if (!checker) {
+    return res.status(403).json({
+      error: "Access Denied!",
+    });
+  }
+  next();
+};
