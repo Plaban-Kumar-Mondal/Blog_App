f27125489a6e0f6abf53584e9c951c0db80bc06f
plaban <plabanmondal02@gmail.com>
2020-08-19 01:30:09 +0530

    Home component add

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <div className="App">
-      <Navbar />
+      <Home />
     </div>
   );
 };
---
+++ client/src/components/Home.js
@@ -0,0 +1,65 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const Home = () => {
+  const footerElement = () => {
+    return (
+      <footer class="page-footer">
+        <div class="container">
+          <div class="row">
+            <div class="col l6 s12">
+              <h5 class="white-text">Footer Content</h5>
+              <p class="grey-text text-lighten-4">
+                You can use rows and columns here to organize your footer
+                content.
+              </p>
+            </div>
+            <div class="col l4 offset-l2 s12">
+              <h5 class="white-text">Links</h5>
+              <ul>
+                <li>
+                  <a class="grey-text text-lighten-3" href="#!">
+                    Link 4
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="footer-copyright">
+          <div class="container">
+            © 2014 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">
+              More Links
+            </a>
+          </div>
+        </div>
+      </footer>
+    );
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <section className="container">
+        <div class="row">
+          <div class="col s12 m6">
+            <div class="card purple darken-1">
+              <div class="card-content white-text">
+                <span class="card-title">Card Title</span>
+                <p>
+                  I am a very simple card. I am good at containing small bits of
+                  information. I am convenient because I require little markup
+                  to use effectively.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {footerElement()}
+    </div>
+  );
+};
+
+export default Home;
