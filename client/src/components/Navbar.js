8a58fb1f967de9de76146d6cbf4e830111f63d5c
plaban <plabanmondal02@gmail.com>
2020-08-18 23:34:52 +0530

    change navbar to class component and add mobile navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,28 +1,55 @@
-import React, { useEffect } from "react";
+import React, { Component } from "react";
 
-const Navbar = () => {
-  return (
-    <nav className="indigo darken-4">
-      <div className="container">
-        <div class="nav-wrapper">
-          <a href="#" class="brand-logo">
-            Blog App
-          </a>
-          <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li>
-              <a href="sass.html">Home</a>
-            </li>
-            <li>
-              <a href="badges.html">Signup</a>
-            </li>
-            <li>
-              <a href="collapsible.html">Login</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
-};
+class Navbar extends Component {
+  componentDidMount() {
+    const M = window.M;
+    document.addEventListener("DOMContentLoaded", function () {
+      var elems = document.querySelectorAll(".sidenav");
+      var instances = M.Sidenav.init(elems, {});
+    });
+  }
+
+  render() {
+    return (
+      <section>
+        <nav className="indigo darken-4">
+          <div className="container">
+            <div class="nav-wrapper">
+              <a href="#" class="brand-logo">
+                Blog App
+              </a>
+              <a className="sidenav-trigger right" data-target="mobile-links">
+                <i className="material-icons">menu</i>
+              </a>
+              <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li>
+                  <a href="sass.html">Home</a>
+                </li>
+                <li>
+                  <a href="badges.html">Signup</a>
+                </li>
+                <li>
+                  <a href="collapsible.html">Login</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <ul className="sidenav" id="mobile-links">
+          <li>
+            <a href="sass.html">Home</a>
+          </li>
+          <li>
+            <a href="badges.html">Signup</a>
+          </li>
+          <li>
+            <a href="collapsible.html">Login</a>
+          </li>
+        </ul>
+      </section>
+    );
+  }
+}
 
 export default Navbar;
