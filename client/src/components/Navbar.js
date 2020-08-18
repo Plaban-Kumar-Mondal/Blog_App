b3c795935c5b4ebfc28853bc21390c262199c02f
plaban <plabanmondal02@gmail.com>
2020-08-18 18:40:55 +0530

    basic navbar add

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,28 @@
 import React, { useEffect } from "react";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <nav className="indigo darken-4">
+      <div className="container">
+        <div class="nav-wrapper">
+          <a href="#" class="brand-logo">
+            Blog App
+          </a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li>
+              <a href="sass.html">Home</a>
+            </li>
+            <li>
+              <a href="badges.html">Signup</a>
+            </li>
+            <li>
+              <a href="collapsible.html">Login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
