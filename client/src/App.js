368b30b278b64ae86e9362fe2cf62e7106f10712
plaban <plabanmondal02@gmail.com>
2020-08-19 02:31:54 +0530

    react-router add to component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <div className="App">
         <Navbar />
         <Route exact path="/" component={Home} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/login" component={Login} />
       </div>
     </BrowserRouter>
   );
