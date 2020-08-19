136c30ef5f0fd3a8297442a660fb3f9a802cf1de
plaban <plabanmondal02@gmail.com>
2020-08-19 17:51:15 +0530

    Profile UI update

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,7 +1,47 @@
 import React from "react";
 
 const Profile = () => {
-  return <div></div>;
+  return (
+    <div className="container" id="profile">
+      <h2 className="welcome-msg">Welcome, Plaban</h2>
+      <h5 className="profile-email">plabanmondal02@gmail.com</h5>
+      <hr />
+      <div className="row">
+        <div className="col s12 m6 center">
+          <button className="waves-effect waves-light btn purple darken-3">
+            Edit Profile
+          </button>
+        </div>
+        <div className="col s12 m6 center">
+          <button className="waves-effect waves-light btn red accent-4">
+            Logout
+          </button>
+        </div>
+      </div>
+
+      <section className="row">
+        <div class="col s12">
+          <div class="card indigo lighten-5">
+            <div class="card-content ">
+              <span class="card-title center-align">Card Title</span>
+              <p>
+                I am a very simple card. I am good at containing small bits of
+                information. I am convenient because I require little markup to
+                use effectively.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <button
+        class="btn-floating btn-large waves-effect waves-light light-green accent-3"
+        id="rigth-btn"
+      >
+        <i class="material-icons">edit</i>
+      </button>
+    </div>
+  );
 };
 
 export default Profile;
